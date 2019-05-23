9a7d86484ceb9e9b732f6afc7c9435a6210ec84c
hcsmgyy <1083562934@qq.com>
2019-05-23 22:29:01 +0800

    不要上传你所有的.class文件 正常commit

--- eshop/it_manager/it_manager_pojo/src/main/java/cn/beans/pojo/TbItemParamItem.java
+++ eshop/it_manager/it_manager_pojo/src/main/java/cn/beans/pojo/TbItemParamItem.java
@@ -1,8 +1,10 @@
 package cn.beans.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class TbItemParamItem {
+public class TbItemParamItem implements Serializable
+{
     private Long id;
 
     private Long itemId;
